925bf51a4a9182b00d15b2332ae8b6c1f858e165
MaksimS <sipatovGT@mail.ru>
2022-10-26 15:11:20 +0400

    ex02Delete2Digit

---
+++ ex02Delete2Digit/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности число и удаляет вторую цифру слева направо этого числа.
+Console.WriteLine("Введите любое неотридцательное, целое число ");
+Console.Write("Введите целое число: ");
+int input = int.Parse(Console.ReadLine()!);
+
+if (input <= 9)
+{
+    Console.WriteLine("Второй цифры нет.");
+}
+else
+{
+    void DeleteSecondDigit(int input)
+    {
+        int sum = 0;
+        int numberPosition = 1;
+        while (input > 99)
+        {
+            int div = input % 10;
+            sum = sum + div * numberPosition;
+            numberPosition = numberPosition * 10;
+            input = input / 10;
+        }
+        input = input / 10;
+        int result = input * numberPosition + sum;
+        Console.WriteLine("Итоговое число после удаления второй цифры: " + result);
+    }
+    DeleteSecondDigit(input);
+}
+
