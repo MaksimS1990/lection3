4305d1f73d88eab9f075d8b6adaa89aadfa9579f
MaksimS <sipatovGT@mail.ru>
2022-10-27 12:26:57 +0400

    ex03maxnumberfunction

--- ex03/Program.cs
+++ ex03/Program.cs
@@ -1,13 +1,25 @@
 ﻿// напишите программу, которая вводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
 Console.WriteLine("Программа вводит любое целое число от 10 до 99 и выводит в консоль наибольшую цифру числа!");
-int randomNumber = new Random().Next(10, 100);
-Console.WriteLine(randomNumber);
-int B = randomNumber % 10;
-int C = randomNumber / 10;
 
-if (B > C){
-    Console.WriteLine("Наибольшее число: " + B);
+int GenerateRandomNumber() {
+   return new Random().Next(10, 100);
 }
-else{
-    Console.WriteLine("Наибольшее число: " + C);
-}
+
+int GetMaxDigitForTwoDigitsNumber(int randomNumber) {
+    Console.WriteLine(randomNumber);
+    int B = randomNumber % 10;
+    int C = randomNumber / 10;
+
+    int max = C;
+
+    if (max > B)
+        max = C;
+    else max = B;
+    
+    return max;
+} 
+int randomNumber = GenerateRandomNumber();
+
+int max = GetMaxDigitForTwoDigitsNumber(randomNumber);
+
+Console.WriteLine("Наибольшая цифра: " + max);
