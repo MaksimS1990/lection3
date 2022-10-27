c973e60752bbb39f557a5ee83342a9b568edfcf7
MaksimS <sipatovGT@mail.ru>
2022-10-27 16:23:51 +0400

    ex05kratnost

---
+++ ex05/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа на вход принимает 2 числа и выводит, является ли второе кратным первому.
+// Если число 2 не кратно числу, то программа выводит остаток от деления.
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+int result = number1 % number2;
+
+if (result == 0){
+    Console.WriteLine("Да, число " + number2 + " кратно числу " + number1);
+}
+else{
+    Console.WriteLine("Нет, число " + number2 + " не кратно числу " + number1);
+}
